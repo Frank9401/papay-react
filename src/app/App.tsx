21b8b5b95880935f684631cc4abb20c720b4974f
Frank9401 <faxriddin0115@gmail.com>
2023-06-28 01:55:57 +0900

    feat: develop routing based on papay project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,39 +2,67 @@ import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import React from 'react';
 import '../css/App.css';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
-import { RippleBadge } from './MaterialTheme/styled';
-import Users from './components/users';
-import Dishes from './components/dishes';
+import { RestaurantPage } from './screens/RestaurantPage';
+import { CommunityPage } from './screens/CommunityPage';
+import { OrderPage } from './screens/OrdersPage';
+import { MemberPage } from './screens/MembersPage';
+import { HelpPage } from './screens/HelpPage';
+import { LoginPage } from './screens/LoginPage';
+import { Homepage } from './screens/Homepage';
 
 function App() {
   return (
     <Router>
       <div>
-        
+
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/restaurant">RestaurantPage</Link>
             </li>
             <li>
-              <Link to="/dishes">Dishes</Link>
+              <Link to="/community">CommunityPage</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">MemberPage</Link>
+            </li>
+            <li>
+              <Link to="/help">HelpPage</Link>
+            </li>
+            <li>
+              <Link to="/login">LoginPage</Link>
+            </li>
+            <li>
+              <Link to="/">Homepage</Link>
             </li>
           </ul>
         </nav>
 
         
         <Switch>
-          <Route path="/dishes">
-            <Dishes />
+          <Route path="/restaurant">
+            <RestaurantPage />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/community">
+            <CommunityPage />
+          </Route>
+          <Route path="/orders">
+            <OrderPage />
+          </Route>
+          <Route path="/member-page">
+            <MemberPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
           </Route>
           <Route path="/">
-            <Home />
+            <Homepage />
           </Route>
         </Switch>
       </div>
---
+++ src/app/screens/OrdersPage/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from '@mui/material';
+
+export function OrderPage() {
+    return (
+        <Container>OrderPage</Container>
+    );
+}
